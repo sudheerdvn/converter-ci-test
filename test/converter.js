ab972701e0fe653f92b2bd7030b3179dd85e2ecf
Sudheer Donaboina <dvn.sudheer@gmail.com>
2020-12-03 00:18:11 -0800

    Intentionally fail a test case

--- test/converter.js
+++ test/converter.js
@@ -12,7 +12,7 @@ describe("Color Code Converter", function() {
 
       expect(redHex).to.equal("ff0000");
       expect(greenHex).to.equal("00ff00");
-      expect(blueHex).to.equal("0000ff");
+      expect(blueHex).to.equal("0000f");
     });
   });
 
